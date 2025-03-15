413c2901843de7817006d3fb4623eb6752121ea5
Yashvardhan <yashvardhansingh1009@gmail.com>
2025-03-15 23:12:50 +0530

    added filter function, flowchart and coding exerise 1

---
+++ 5-Flowchart_and_problem_solving/Pseudocode.py
@@ -0,0 +1,14 @@
+'''
+
+Begin
+    Input FirstNumber
+    Input SecondNumber
+
+    SUM = FirstNumber + SecondNumber
+
+    print SUM
+
+End
+
+'''
+
---
+++ 6-Pattern_practice_questions/test.py
@@ -0,0 +1,4 @@
+def generate_square_pattern(n):
+    return ['*' * n for i in range(n)]
+
+print(generate_square_pattern(3))
