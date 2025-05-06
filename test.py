0c513e1a41a87b633dc0c545f5d8cc4095ffcd57
Yashvardhan <yashvardhansingh1009@gmail.com>
2025-05-06 21:35:48 +0530

    Add new section 14 and codingExercise75

--- test.py
+++ test.py
@@ -1,19 +1,4 @@
-def max_subarray_sum(arr):
-    if not arr:
-        return 0
- 
-    # Initialize variables
-    max_ending_here = 0
-    max_so_far = float('-inf')
- 
-    # Iterate through the array
-    for x in arr:
-        # Update max_ending_here to include current element
-        max_ending_here = max(x, max_ending_here + x)
-        # Update max_so_far to the maximum value found so far
-        max_so_far = max(max_so_far, max_ending_here)
- 
-    return max_so_far
+lst_arr = [[2], [2, 3, 5]]
 
-arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print("Maximum sub array sum is : ", max_subarray_sum(arr))
+print(len(lst_arr[-1]))
+
